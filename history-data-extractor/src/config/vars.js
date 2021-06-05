2fb0d41accdb8c1264881e09e16adea0d1469b43
Jesús Genil <prgrav03@outsourcing.cepsa.com>
2021-06-05 14:07:08 +0200

    feat: add sites

--- history-data-extractor/src/config/vars.js
+++ history-data-extractor/src/config/vars.js
@@ -9,5 +9,15 @@ module.exports = {
     solar: "uvIndex",
     weather:
       "airTemperature,cloudCover,currentSpeed,gust,humidity,swellHeight,swellPeriod,visibility,waterTemperature,waveHeight,wavePeriod,windSpeed"
+  },
+  sites: {
+    puntaHumbria: {
+      length: 37.178056,
+      latitud: -6.971944
+    },
+    marbella: {
+      length: 36.506111,
+      latitud: -4.883889
+    }
   }
 };
