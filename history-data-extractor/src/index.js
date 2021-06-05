5b12d7f4bf52299c94960ba28fd59a2e2260eb09
Jesús Genil <prgrav03@outsourcing.cepsa.com>
2021-06-05 19:51:28 +0200

    feat: main process

--- history-data-extractor/src/index.js
+++ history-data-extractor/src/index.js
@@ -1,1 +1,19 @@
-console.info("Started!");
+const DataService = require("./services");
+const vars = require("./config/vars");
+
+/**
+ * For every site stored in project configuration, execute Get data from
+ * storm glass Api and send te correlated information to DB writer module
+ * exposed by an http destination endpoint.
+ */
+const main = async () => {
+  console.info("Starting historical data recollection");
+  const promises = Object.keys(vars.sites).map(async siteKey => {
+    const data = await DataService.getData(vars.sites[siteKey]);
+    await DataService.sendData(data);
+  });
+  await Promise.all(promises);
+  console.info("Finished historical data recollection");
+};
+
+main();
