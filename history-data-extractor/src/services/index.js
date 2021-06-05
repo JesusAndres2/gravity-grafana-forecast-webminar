66c7bf1cf986ccc589ee9d29e4c59130b315ea5d
Jesús Genil <prgrav03@outsourcing.cepsa.com>
2021-06-05 19:20:43 +0200

    refactor: services splitted

--- history-data-extractor/src/services/index.js
+++ history-data-extractor/src/services/index.js
@@ -1,22 +1,5 @@
-const RestServices = require("../rest-services");
-const mapData = require("../mappers");
+const getData = require("./get-data");
 
-module.exports = site => new Promise((resolve, reject) => {
-    console.info(`Getting forecast historical day data of site: ${JSON.stringify(site)}`);
-    const promises = [
-      RestServices.getSolarData(site.length, site.latitude),
-      RestServices.getWeatherData(site.length, site.latitude)
-    ];
-    Promise.all(promises).then(([solarData, weatherData]) => {
-      console.info("Retrieved forecast data. Starting to map it");
-      const result = mapData(weatherData, solarData, site);
-      console.info(`Mapping finished successfully. Result: ${JSON.stringify(result)}`);
-      resolve(result);
-    })
-    .catch(error => {
-        console.error(
-          `Error retrieving forecast historical data of site ${JSON.stringify(site)}. Stack trace: ${error}`
-        );
-        reject(error);
-    });
-});
+module.exports = {
+    getData
+}
