be87442a0c937faa834ee33c3547635545fedc87
Jesús Genil <prgrav03@outsourcing.cepsa.com>
2021-06-05 13:21:24 +0200

    feat: add base config

---
+++ history-data-extractor/src/config/vars.js
@@ -0,0 +1,12 @@
+module.exports = {
+  baseUrl: "https://api.stormglass.io/v2/",
+  apiKey: "",
+  paths: {
+    solar: "solar/",
+    weather: "weather/"
+  },
+  params: {
+    solar: "",
+    weather: ""
+  }
+};
