a43a8f83af3013ed5347a452e06ec0415da5c408
Jesús Genil <prgrav03@outsourcing.cepsa.com>
2021-06-06 22:05:51 +0200

    feat: custom http-responses

---
+++ db-writer/src/utils/http-responses.js
@@ -0,0 +1,32 @@
+/*
+  Generic Error Response
+*/
+const GENERIC_ERROR_RESPONSE = {
+    statusCode: 500,
+    body: JSON.stringify({
+        message: "Something was wrong, server error",
+        error: "org.dbwriter.InternalError"
+    })
+};
+
+/**
+  No request treatment
+*/
+const BAD_REQUEST_ERROR_RESPONSE = {
+    statusCode: 400,
+    body: JSON.stringify({
+        message: "Bad Request Error (blame the client)",
+        error: "org.dbwriter.BadRequestError"
+    })
+};
+
+const OK_RESPONSE = {
+    statusCode: 200,
+    body: ""
+};
+
+module.exports = {
+    GENERIC_ERROR_RESPONSE,
+    BAD_REQUEST_ERROR_RESPONSE,
+    OK_RESPONSE
+};
