a265c826e4ec5ca2a3621fcf72c2becc591808a9
Jesús Genil <prgrav03@outsourcing.cepsa.com>
2021-06-06 21:33:13 +0200

    feat: db init

---
+++ db-writer/src/config/vars.js
@@ -0,0 +1,10 @@
+module.exports = {
+    postgres: {
+        dbname: "postgres",
+        username: "postgres",
+        password: "postgres",
+        host: "localhost",
+        engine: "postgres",
+        port: 5432
+    }
+};
